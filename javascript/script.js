9aeda45d9e5e13fcdc08a66377e18b0ec402b50b
dinnagonzales <dinnagonzales.05@gmail.com>
2014-07-17 21:30:50 -0700

    about me

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-   var $a = $('a#fourthbox'), $targets = $('div.targets');
-   $a.on('click', function(e) {
-      e.preventDefault(); 
-      var i = $a.index(this);
-      var offset = $targets.eq(++i).offset().top;
-      $('html, body').stop().animate({ scrollTop: offset }, 400);
-   });
-});
